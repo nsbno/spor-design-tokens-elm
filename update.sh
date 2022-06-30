a3b92fa9f2095d90c93d93846a64b2dd243038b1
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-06-30 11:09:04 +0200

    Make sure version field in elm.json is retained.

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+CURRENT_ELM_VERSION=$(jq .version elm.json)
+
 # Clean current repo
 
 rm -r src || true
@@ -27,6 +29,8 @@ popd
 # Copy generated elm package to root
 
 cp -R spor/packages/spor-design-tokens/elm/ .
+jq --indent 4 ".version = $CURRENT_ELM_VERSION" elm.json > elm2.json
+mv elm2.json elm.json
 
 # Cleanup
 
